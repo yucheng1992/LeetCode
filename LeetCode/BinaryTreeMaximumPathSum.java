83ff9adb1f8e0668ac0e38b98f660c3c006c7cfc
yl2695 <yl2695@nyu.edu>
2015-01-12 21:34:39 +0800

    commit

---
+++ LeetCode/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeMaximumPathSum {
+    public int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) return 0;
+        helper(root);
+        return max;
+    }
+    public int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        max = Math.max(max, left + right + root.val);
+        return Math.max(left, right) + root.val;
+    }
+}
