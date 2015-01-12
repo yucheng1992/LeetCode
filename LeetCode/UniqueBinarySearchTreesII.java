8a0ec50243749fcb8aef0ba258ad204b1ee19e50
yl2695 <yl2695@nyu.edu>
2015-01-12 12:15:18 +0800

    commit

---
+++ LeetCode/UniqueBinarySearchTreesII.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; left = null; right = null; }
+ * }
+ */
+public class UniqueBinarySearchTreesII {
+    public List<TreeNode> generateTrees(int n) {
+        return helper(1, n);
+        }
+    public List<TreeNode> helper(int start, int end) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> lefts = helper(start, i-1);
+            List<TreeNode> rights = helper(i+1, end);
+            for (TreeNode leftNode: lefts) {
+                for (TreeNode rightNode: rights) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = leftNode;
+                    node.right = rightNode;
+                    list.add(node);
+                }
+            }
+        }
+        return list;
+    }
+}
