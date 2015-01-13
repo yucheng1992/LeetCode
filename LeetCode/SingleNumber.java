d12f123ba8c9677d760d116a2748dfec8dd2c12f
yl2695 <yl2695@nyu.edu>
2015-01-14 00:59:45 +0800

    commit

---
+++ LeetCode/SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        int res = A[0];
+        for (int i = 1; i < A.length; i++) {
+            res ^= A[i];
+        }
+        return res;
+    }
+}
