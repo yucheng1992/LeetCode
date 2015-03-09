97c4ffe452400a14d5eccc2dba9821d7903f1fc7
yl2612 <yl2695@nyu.edu>
2015-03-09 16:24:35 -0400

    change Fac

--- LeetCode/FactorialTrailingZeroes.java
+++ LeetCode/FactorialTrailingZeroes.java
@@ -1,12 +1,10 @@
 public class FactorialTrailingZeroes {
     public int trailingZeroes(int n) {
-        int max = (int)(Math.log(n) / Math.log(5));
-        int i = 1;
-        int count = 0;
-        while (i <= max) {
-            count += n / (int)Math.pow(5, i);
-            i++;
+        int log = (int)(Math.log(n) / Math.log(5));
+        int res = 0;
+        for (int i = 1; i <= log; i++) {
+            res += n / (int)Math.pow(5, i);
         }
-        return count;
+        return res;
     }
 }
