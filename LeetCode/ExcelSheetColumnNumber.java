073e7a359843c838045d8fa5aed6039e9d9f83e2
yl2612 <yl2695@nyu.edu>
2015-03-09 21:21:04 -0400

    commit

--- LeetCode/ExcelSheetColumnNumber.java
+++ LeetCode/ExcelSheetColumnNumber.java
@@ -1,10 +1,9 @@
 class ExcelSheetColumnNumber {
     public int titleToNumber(String s) {
-        if (s == null || s.length() == 0) return 0;
         int len = s.length();
         int count = 0;
         for (int i=0; i<len; i++) {
-            count += (s.charAt(i) - 'A' + 1) * Math.pow(26, len-i-1);
+            count += (s.charAt(i) - 'A' + 1) * Math.pow(26, len - i - 1);
         }
         return count;
     }
