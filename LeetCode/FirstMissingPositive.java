5571957e855eacbdc5788c5fcf97573a057cbf36
yl2612 <yl2695@nyu.edu>
2015-03-12 11:43:42 -0400

    commit

--- LeetCode/FirstMissingPositive.java
+++ LeetCode/FirstMissingPositive.java
@@ -18,4 +18,24 @@ public class FirstMissingPositive {
         }
         return max + 1;
     }
+    
+    public int firstMissingPositive2(int[] A) {
+        if (A == null || A.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] <= A.length && A[i] > 0 && A[A[i] - 1] != A[i]) {
+                int temp = A[A[i] - 1];
+                A[A[i] - 1] = A[i];
+                A[i] = temp;
+                i--;
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return A.length + 1;
+    }
 }
