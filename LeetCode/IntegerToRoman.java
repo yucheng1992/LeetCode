037960ef956c9bc7a9d637850cbdf8ec162b9bed
yl2612 <yl2695@nyu.edu>
2015-03-15 20:19:09 -0400

    commit

--- LeetCode/IntegerToRoman.java
+++ LeetCode/IntegerToRoman.java
@@ -1,5 +1,19 @@
 public class IntegerToRoman {
+    
     public String intToRoman(int num) {
+        String res = "";
+        String[] str = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        for (int i = 0; num != 0; i++) {
+            while (num >= value[i]) {
+                num -= value[i];
+                res += str[i];
+            }
+        }
+        return res;
+    }
+    
+    public String intToRoman2(int num) {
         StringBuffer res = new StringBuffer();
         int thu = num / 1000;
         for (int i=0; i<thu; i++) {
