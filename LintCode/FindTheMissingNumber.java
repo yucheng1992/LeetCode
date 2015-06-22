87b18d69515185af66c9faf72136077e7f92ee22
yl2612 <yl2695@nyu.edu>
2015-06-22 13:31:58 -0400

    find the missing number

---
+++ LintCode/FindTheMissingNumber.java
@@ -0,0 +1,23 @@
+public class FindTheMissingNumber {
+    /**    
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int findMissing(int[] nums) {
+        // write your code here
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums.length && nums[nums[i]] != nums[i]) {
+                int temp = nums[nums[i]];
+                nums[nums[i]] = nums[i];
+                nums[i] = temp;
+                i--;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
