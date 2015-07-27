98bcd48bb23feeac3b6eb6f3af7afa4b74105454
yl2612 <yl2695@nyu.edu>
2015-07-27 16:39:26 -0400

    search a 2d matrix II

---
+++ LeetCode/SearchA2DMatrixII.java
@@ -0,0 +1,16 @@
+public class SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
