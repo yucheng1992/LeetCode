fa46e577cf726edc5e4a5e75480b826a4c66fb71
yl2612 <yl2695@nyu.edu>
2015-08-18 16:19:04 -0400

    ugly number

---
+++ LeetCode/UglyNumber.java
@@ -0,0 +1,17 @@
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }  
+        while (num % 5 == 0) {
+            num = num / 5;
+        }
+        while (num % 3 == 0) {
+            num = num / 3;
+        }
+        while (num % 2 == 0) {
+            num = num / 2;
+        }
+        return num == 1;
+    }
+}
