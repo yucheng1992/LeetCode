366c2ff1998f8bfe185422120889fb077d3b9b5d
yl2612 <yl2695@nyu.edu>
2015-08-27 15:48:12 -0400

    binary tree maximum path sum

---
+++ LintCode/Recursion/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,35 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class BinaryTreeMaximumPathSum {
+    
+    private int max = Integer.MIN_VALUE; 
+    
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxPathSum(TreeNode root) {
+        // write your code here
+        helper(root);
+        return max;
+    }
+    
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        max = Math.max(max, left + right + root.val);
+        return Math.max(left, right) + root.val;
+    }
+}
