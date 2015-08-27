25e4b87ef9367a6772bc3ab2136ab6d1236540e1
yl2612 <yl2695@nyu.edu>
2015-08-27 14:33:36 -0400

    code style

--- LeetCode/GrayCode.java
+++ LeetCode/GrayCode.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 public class GrayCode {
     public List<Integer> grayCode(int n) {
-        if(n==0) {
+        if (n == 0) {
             List<Integer> result = new ArrayList<Integer>();
             result.add(0);
             return result;
         }
 
-        List<Integer> tmp = grayCode(n-1);
-        int addNumber = 1 << (n-1);
+        List<Integer> tmp = grayCode(n - 1);
+        int addNumber = 1 << (n - 1);
         List<Integer> result = new ArrayList<Integer>(tmp);
-        for(int i=tmp.size()-1;i>=0;i--) {
+        for(int i = tmp.size() - 1; i >= 0; i--) {
             result.add(addNumber + tmp.get(i));
         }
         return result;
