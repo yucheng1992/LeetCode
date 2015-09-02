e25195be3883404b7ed99d7af653ddf9be83ed60
yl2612 <yl2695@nyu.edu>
2015-09-02 11:20:09 -0400

    get rid of global variable

--- LeetCode/LetterCombinationsOfAPhoneNumber.java
+++ LeetCode/LetterCombinationsOfAPhoneNumber.java
@@ -1,24 +1,22 @@
 public class LetterCombinationsOfAPhoneNumber  {
-    List<String> res = new ArrayList<String>();
     public List<String> letterCombinations(String digits) {
+        String[] digitStr = new String[]{" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new LinkedList<String>();
         if (digits == null || digits.length() == 0) {
-            res.add("");
             return res;
         }
-        dfs(digits, 0, "");
+        helper(digitStr, 0, digits, "", res);
         return res;
     }
-    public void dfs(String digits, int index, String cur) {
-        if (index == digits.length()) {
+    
+    private void helper(String[] digitStr, int level, String digits, String cur, List<String> res) {
+        if (level == digits.length()) {
             res.add(cur);
             return;
         }
-        String[] button = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        int digit = Character.getNumericValue(digits.charAt(index));
-        for (int i=0; i<button[digit].length(); i++) {
-            String newCur = cur;
-            newCur += button[digit].charAt(i);
-            dfs(digits, index+1, newCur);
+        String candidate = digitStr[Character.getNumericValue(digits.charAt(level))];
+        for (int i = 0; i < candidate.length(); i++) {
+            helper(digitStr, level + 1, digits, cur + candidate.charAt(i), res);
         }
-    }
+    } 
 }
