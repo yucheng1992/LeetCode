ca68bbf4825539be5160d12e91bfe39174e23f56
yl2612 <yl2695@nyu.edu>
2015-09-10 14:48:39 -0400

    h-index

---
+++ LeetCode/HIndex.java
@@ -0,0 +1,14 @@
+public class HIndex {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
