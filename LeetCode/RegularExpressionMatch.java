0b3418f19941c61d9b63503e8de2f74e91db17f8
yl2612 <yl2695@nyu.edu>
2015-09-14 18:06:40 -0400

    add another solution for regular expression match

--- LeetCode/RegularExpressionMatch.java
+++ LeetCode/RegularExpressionMatch.java
@@ -41,4 +41,64 @@ public class RegularExpressionMatch {
         }
         return dp[s.length()][p.length()];
     }
+
+    // another DP solution
+    public boolean isMatch2(String s, String p) {
+        if (s.length() == 0 && p.length() == 0) {
+            return true;
+        }
+        if (p.length() == 0) {
+            return false;
+        }
+        if (p.equals(".*")) {
+            return true;
+        }
+        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+        dp[0][0] = true;
+        for (int j = 0; j <= s.length(); j++) {
+            for (int i = 0; i <= p.length(); i++) {
+                if (dp[i][j]) {
+                    continue;
+                }
+                if (j == 0) {
+                    if (p.charAt(i-1) != '*') {
+                        dp[i][j] = false;
+                    } else {
+                        dp[i][j] = dp[i - 2][j];
+                    }
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i][j] = false;
+                    continue;
+                }
+                if (p.charAt(i-1) == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(i-1) == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i-2][j];
+                    if (dp[i][j] && p.charAt(i-2) == s.charAt(j-1)) {
+                        for (int k = j + 1; k <= s.length(); k++) {
+                            if (s.charAt(k-1) == s.charAt(k - 2)) {
+                                dp[i][k] = true;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    if (p.charAt(i - 2) == '.') {
+                        for (int k = j + 1; k <= s.length(); k++) {
+                            dp[i][k] = dp[i][k - 2];
+                        }
+                    }
+                } else {
+                    if (s.charAt(j-1) == p.charAt(i-1)) {
+                        dp[i][j] = dp[i-1][j-1];
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[p.length()][s.length()];
+    }
 }
