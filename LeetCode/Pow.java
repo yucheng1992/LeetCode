d0444958e3ebf00ce5624574a4a321f145056874
yl2612 <yl2695@nyu.edu>
2015-09-28 17:08:25 -0400

    make code in pow more clean

--- LeetCode/Pow.java
+++ LeetCode/Pow.java
@@ -1,18 +1,21 @@
 public class Pow {
     public double pow(double x, int n) {
-        if (n == 0 || x == 1.0 || x == -1.0 && Math.abs(n)%2 == 0) {
+        if (n == 0) {
             return 1.0;
-        } 
-        if (x == -1.0 && Math.abs(n)%2 == 1) {
-            return -1.0;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n == Integer.MIN_VALUE) {
+            return 1 / (x * myPow(x, -(n + 1)));
         }
         if (n < 0) {
-            return 1.0 / pow(x, -n);
+            return 1 / myPow(x, -n);
         }
-        if (n % 2 == 0) {
-            return pow(x * x, n / 2);
+        if (n % 2 == 1) {
+            return x * myPow(x * x, n / 2);
         } else {
-            return x * pow(x * x, (n-1) / 2);
+            return myPow(x * x, n / 2);
         }
     }
 }
