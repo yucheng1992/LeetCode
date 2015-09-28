0132c4992de079d69c0067b2d0d7f9ba5799ab5b
yl2612 <yl2695@nyu.edu>
2015-09-28 14:10:37 -0400

    find the duplicate number

---
+++ LeetCode/FindTheDuplicateNumber.java
@@ -0,0 +1,21 @@
+public class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int left = 1;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) {
+                    count += 1;
+                }
+            }
+            if (count > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
