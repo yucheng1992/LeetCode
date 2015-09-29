c5faeb8f0cc3a4b42b882ebb0d4bae81662fce41
yl2612 <yl2695@nyu.edu>
2015-09-29 16:57:08 -0400

    make code in merge intervals cleaner

--- LeetCode/MergeIntervals.java
+++ LeetCode/MergeIntervals.java
@@ -7,60 +7,42 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-public class MergeIntervals {
+public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        List<Interval> merged = new ArrayList<Interval>();
-        int i = 0;
-        if (intervals.size() == 1){
-            return intervals;
+        List<Interval> res = new LinkedList<Interval>();
+        if (intervals.size() == 0) {
+            return res;
         }
-        Collections.sort(intervals, new Comparator<Interval>() {
-        @Override
-            public int compare(Interval i1, Interval i2) {
-                return Integer.compare(i1.start, i2.start);
+        Collections.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
             }
         });
-        while (i < intervals.size()) {
-            for (int j=i+1; j<i+2; j++) {
-                if (j >= intervals.size()) {
-                    merged.add(intervals.get(i));
-                } else{
-                    if (canMerge(intervals.get(i), intervals.get(j))) {
-                        Interval newInterval = mergeIntervals(intervals.get(i), intervals.get(j));
-                        intervals.add(i, newInterval);
-                        intervals.remove(i+1);
-                        intervals.remove(i+1);
-                        i--;
-                    } else {
-                        merged.add(intervals.get(i));
-                    }
-                }
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (overlap(intervals.get(i), intervals.get(i + 1))) {
+                Interval cur = mergeTwoInterval(intervals.get(i), intervals.get(i + 1));
+                intervals.remove(i);
+                intervals.remove(i);
+                intervals.add(i, cur);
+                i--;
+            } else {
+                res.add(intervals.get(i));
             }
-            i++;
         }
-        return merged;
+        res.add(intervals.get(intervals.size() - 1));
+        return res;
     }
     
-    public boolean canMerge(Interval a, Interval b) {
-        if (a.end - b.start < 0 || b.end - a.start < 0) {
+    public boolean overlap(Interval a, Interval b) {
+        if (a.start > b.end || b.start > a.end) {
             return false;
         } else {
             return true;
         }
     }
     
-    public Interval mergeIntervals(Interval a, Interval b) {
-        Interval target = new Interval();
-        if (a.start < b.start) {
-            target.start = a.start;
-        } else {
-            target.start = b.start;
-        }
-        if (a.end < b.end) {
-            target.end = b.end;
-        } else {
-            target.end = a.end;
-        }
-        return target;
+    public Interval mergeTwoInterval(Interval a, Interval b) {
+        return new Interval(a.start, Math.max(a.end, b.end));
     }
 }
