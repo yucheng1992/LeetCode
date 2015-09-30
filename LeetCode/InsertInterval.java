b0d1c0226b9a7b754f88c81100da463d660af274
yl2612 <yl2695@nyu.edu>
2015-09-30 12:57:38 -0400

    make complecity O(N) instead of O(NlogN)

--- LeetCode/InsertInterval.java
+++ LeetCode/InsertInterval.java
@@ -1,4 +1,5 @@
-/** Dnition for an interval.
+/**
+ * Definition for an interval.
  * public class Interval {
  *     int start;
  *     int end;
@@ -8,62 +9,55 @@
  */
 public class InsertInterval {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        intervals.add(newInterval);
-        return merge(intervals);
+        return merge(intervals, newInterval);
     }
-    public List<Interval> merge(List<Interval> intervals) {
-        List<Interval> merged = new ArrayList<Interval>();
-        int i = 0;
-        if (intervals.size() == 1){
-            return intervals;
+    public List<Interval> merge(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<Interval>();
+        if (intervals.size() == 0){
+            res.add(newInterval);
+            return res;
         }
-        Collections.sort(intervals, new Comparator<Interval>() {
-        @Override
-            public int compare(Interval i1, Interval i2) {
-                return Integer.compare(i1.start, i2.start);
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (newInterval.start >= intervals.get(i).start && newInterval.start <= intervals.get(i + 1).start) {
+                intervals.add(i + 1, newInterval);
+                break;
             }
-        });
-        while (i < intervals.size()) {
-            for (int j=i+1; j<i+2; j++) {
-                if (j >= intervals.size()) {
-                    merged.add(intervals.get(i));
-                } else{
-                    if (canMerge(intervals.get(i), intervals.get(j))) {
-                        Interval newInterval = mergeIntervals(intervals.get(i), intervals.get(j));
-                        intervals.add(i, newInterval);
-                        intervals.remove(i+1);
-                        intervals.remove(i+1);
-                        i--;
-                    } else {
-                        merged.add(intervals.get(i));
-                    }
+            if (i == intervals.size() - 2) {
+                if (newInterval.start >= intervals.get(intervals.size() - 1).start) {
+                    intervals.add(intervals.size(), newInterval);
+                } else {
+                    intervals.add(0, newInterval);
                 }
+                break;
             }
-            i++;
         }
-        return merged;
+        if (intervals.size() == 1) {
+            if (newInterval.start < intervals.get(0).start) {
+                intervals.add(0, newInterval);
+            } else {
+                intervals.add(1, newInterval);
+            }
+        }
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (canMerge(intervals.get(i), intervals.get(i + 1))) {
+                Interval cur = mergeIntervals(intervals.get(i), intervals.get(i + 1));
+                intervals.remove(i);
+                intervals.remove(i);
+                intervals.add(i, cur);
+                i--;
+            } else {
+                res.add(intervals.get(i));
+            }
+        }
+        res.add(intervals.get(intervals.size() - 1));
+        return res;
     }
     
     public boolean canMerge(Interval a, Interval b) {
-        if (a.end - b.start < 0 || b.end - a.start < 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return !(a.end < b.start || b.end < a.start); 
     }
     
     public Interval mergeIntervals(Interval a, Interval b) {
-        Interval target = new Interval();
-        if (a.start < b.start) {
-            target.start = a.start;
-        } else {
-            target.start = b.start;
-        }
-        if (a.end < b.end) {
-            target.end = b.end;
-        } else {
-            target.end = a.end;
-        }
-        return target;
+        return new Interval(Math.min(a.start, b.start), Math.max(a.end, b.end));
     }
 }
