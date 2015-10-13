95abbb6843dbcc6f1f8ed707205523b6252262bf
yl2612 <yl2695@nyu.edu>
2015-10-13 12:54:54 -0400

    make code in Decode Ways cleaner

--- LeetCode/DecodeWays.java
+++ LeetCode/DecodeWays.java
@@ -1,33 +1,26 @@
 public class DecodeWays {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+        if (s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
-        int[] dp = new int[s.length()];
-        dp[0] = 1;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 0;
+        dp[1] = 1;
         for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i-1) == '0' && s.charAt(i) == '0') {
-                return 0;
-            } else if (s.charAt(i) == '0') {
-                if (s.charAt(i-1) >= '3') {
+            if (s.charAt(i) == '0') {
+                if (s.charAt(i - 1) > '2' || s.charAt(i - 1) == '0') {
                     return 0;
                 } else {
-                    dp[i] = i >= 2 ? dp[i-2] : 1;
-                }
-            } else if (s.charAt(i-1) == '0') {
-                dp[i] = dp[i-1];
-            } else if (s.charAt(i-1) == '1') {
-                dp[i] = dp[i-1] + (i >= 2 ? dp[i-2] : 1);
-            } else if (s.charAt(i-1) == '2') {
-                if (s.charAt(i) >= '7') {
-                    dp[i] = dp[i-1];
-                } else {
-                    dp[i] = dp[i-1] + (i >= 2 ? dp[i-2] : 1);
+                    dp[i + 1] = i == 1 ? 1: dp[i - 1];
                 }
             } else {
-                dp[i] = dp[i-1];
+                if (s.charAt(i - 1) <= '1' && s.charAt(i - 1) > '0'|| s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+                    dp[i + 1] = i == 1 ? 2 : dp[i] + dp[i - 1];
+                } else {
+                    dp[i + 1] = dp[i];
+                }
             }
         }
-        return dp[s.length()-1];
+        return dp[s.length()];
     }
 }
