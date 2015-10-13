6ab93a4c6624769805530a88d45f7e930cb3bc76
yl2612 <yl2695@nyu.edu>
2015-10-13 17:38:19 -0400

    make code in binary tree level order traversal I, II cleaner

--- LeetCode/BinaryTreeLevelOrderTraversal.java
+++ LeetCode/BinaryTreeLevelOrderTraversal.java
@@ -15,24 +15,20 @@ public class BinaryTreeLevelOrderTraversal {
         }
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
-        int preSize = 1;
         while (!queue.isEmpty()) {
             List<Integer> cur = new ArrayList<Integer>();
-            int curSize = 0;
-            for (int i = 0; i < preSize; i++) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 cur.add(node.val);
                 if (node.left != null) {
                     queue.add(node.left);
-                    curSize++;
                 }
                 if (node.right != null) {
                     queue.add(node.right);
-                    curSize++;
                 }
             }
             res.add(cur);
-            preSize = curSize;
         }
         return res;
     }
