a9ea140f8b31c430cc4c4d24ae3fa1f8d8fc3742
yl2612 <yl2695@nyu.edu>
2015-10-15 16:42:05 -0400

    add bit manupilation method for single number II

--- LeetCode/SingleNumberII.java
+++ LeetCode/SingleNumberII.java
@@ -17,4 +17,21 @@ public class SingleNumberII {
         }
         return 0;
     }
+
+    // Bit manipulation
+    public int singleNumberII(int[] nums) {
+        int res = 0;
+        int k = 0;
+        while (k < 32) {
+            int temp = 0;
+            for (int i = 0; i < nums.length; i++) {
+                temp += ((nums[i] >> k) & 1);
+            }
+            if (temp % 3 != 0) {
+                res = res | 1 << k;
+            }
+            k++;
+        }
+        return res;
+    }
 }
