b6dd8e9b815b2551539b261be1eb4305d61cfd9b
yl2612 <yl2695@nyu.edu>
2015-10-17 18:50:21 -0400

    rewrite house robber II

--- LeetCode/CourseSchedule.java
+++ LeetCode/CourseSchedule.java
@@ -32,4 +32,35 @@ public class CourseSchedule {
         }
         return true;
     }
+
+    //BFS
+    public boolean canFinishII(int numCourses, int[][] prerequisites) {
+        int[] degree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            degree[prerequisites[i][0]]++;
+        }
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (prerequisites[i][1] == course) {
+                    degree[prerequisites[i][0]] -= 1;
+                    if (degree[prerequisites[i][0]] == 0) {
+                        queue.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- LeetCode/HouseRobberII.java
+++ LeetCode/HouseRobberII.java
@@ -1,40 +1,19 @@
 public class HouseRobberII {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        } else if (nums.length == 1) {
+        if (nums.length == 1) {
             return nums[0];
         }
-        int[] dp1 = new int[nums.length-1];
-        dp1[0] = nums[0];
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (i == 1) {
-                dp1[i] = dp1[0];
-            } else {
-                dp1[i] = Math.max(nums[i] + dp1[i-2], dp1[i-1]);
-            }
+        return Math.max(dp(nums, 0, nums.length - 2), dp(nums, 1, nums.length - 1));
+    }
+    
+    private int dp(int[] nums, int low, int high) {
+        int preRob = 0, preNotRob = 0, rob = 0, notRob = 0;
+        for (int i = low; i <= high; i++) {
+            rob = preNotRob + nums[i];
+            notRob = Math.max(preRob, preNotRob);
+            preRob = rob;
+            preNotRob = notRob;
         }
-        int[] dp2 = new int[nums.length-1];
-        dp2[nums.length-2] = nums[nums.length-1];
-        for (int i = nums.length - 3; i >= 0; i--) {
-            if (i == nums.length - 3) {
-                dp2[i] = dp2[nums.length-2];
-            } else {
-                dp2[i] = Math.max(nums[i+1] + dp2[i+2], dp2[i+1]);
-            }
-        }
-        if (nums.length == 2) {
-            return Math.max(dp1[nums.length-2], dp2[0]);
-        }
-        int[] dp3 = new int[nums.length-2];
-        dp3[0] = nums[1];
-        for (int i = 2; i < nums.length - 1; i++) {
-            if (i == 2) {
-                dp3[i-1] = Math.max(dp3[0], nums[i]);
-            } else {
-                dp3[i-1] = Math.max(dp3[i-2], nums[i] + dp3[i-3]);
-            }
-        }
-        return Math.max(Math.max(dp1[nums.length-2], dp2[0]), dp3[nums.length-3]);
+        return Math.max(rob, notRob);
     }
 }
