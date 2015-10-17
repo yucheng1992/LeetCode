e41753392d4a6f1166b02722743684491e0824d2
yl2612 <yl2695@nyu.edu>
2015-10-16 20:20:53 -0400

    rewrite find peak number

--- LeetCode/FindPeakElement.java
+++ LeetCode/FindPeakElement.java
@@ -1,32 +1,16 @@
 public class FindPeakElement {
     public int findPeakElement(int[] num) {
-        if (num.length == 1) {
-            return 0;
-        }
         int l = 0;
         int r = num.length-1;
-        while (l < r) {
+        while (l <= r) {
+            if (l == r) {
+                break;
+            }
             int m = (l + r) / 2;
-            if (m == 0) {
-                if (num[m] > num[m+1]) {
-                    return m;
-                } else {
-                    return m+1;
-                }
-            }
-            if (m == num.length-1) {
-                if (num[m] > num[m-1]) {
-                    return m;
-                } else {
-                    return m-1;
-                }
-            }
-            if (num[m] > num[m+1] && num[m] > num[m-1]) {
-                return m;
-            } else if (num[m] < num[m+1] && num[m-1] < num[m]) {
+            if (num[m] < num[m + 1]) {
                 l = m + 1;
             } else {
-                r = m - 1;
+                r = m;
             }
         }
         return l;
