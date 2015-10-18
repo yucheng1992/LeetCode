248afd21f6a682e77c5b24fa91926eda8c64c988
yl2612 <yl2695@nyu.edu>
2015-10-18 16:50:28 -0400

    rewrite LowestCommonAncestorOfABinarySearchTree.java

--- LeetCode/LowestCommonAncestorOfABinarySearchTree.java
+++ LeetCode/LowestCommonAncestorOfABinarySearchTree.java
@@ -9,25 +9,15 @@
  */
 public class LowestCommonAncestorOfABinarySearchTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while (root != null) {
-            if (containsNode(root.left, p) && containsNode(root.left, q)) {
-                root = root.left;
-            } else if (containsNode(root.right, p) && containsNode(root.right, q)) {
-                root = root.right;
-            } else {
-                break;
-            }
-        }
-        return root;
-    }
-    
-    public boolean containsNode(TreeNode root, TreeNode node) {
         if (root == null) {
-            return false;
-        } else if (root.equals(node)) {
-            return true;
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
         } else {
-            return containsNode(root.left, node) || containsNode(root.right, node);
+            return root;
         }
     }
 }
