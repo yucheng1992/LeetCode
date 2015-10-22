ca1273bbf8af5ad839a10f575aee8dd2a95af3fe
yl2612 <yl2695@nyu.edu>
2015-10-21 22:01:07 -0400

    rewrite wiggle sort

--- LeetCode/WiggleSort.java
+++ LeetCode/WiggleSort.java
@@ -1,29 +1,11 @@
 public class WiggleSort {
     public void wiggleSort(int[] nums) {
-        if (nums.length <= 1) {
-            return;
-        } else if (nums.length == 2) {
-            if (nums[0] > nums[1]) {
-                int temp = nums[1];
-                nums[1] = nums[0];
-                nums[0] = temp;
-                return;
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 == 1 && nums[i] < nums[i - 1] || i % 2 == 0 && nums[i] > nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
             }
-        }
-        boolean lessOrder = true;
-        for (int i = 0; i < nums.length - 2; i++) {
-            int[] temp = new int[]{nums[i], nums[i+1], nums[i+2]};
-            Arrays.sort(temp);
-            if (lessOrder) {
-                nums[i] = temp[0];
-                nums[i+1] = temp[2];
-                nums[i+2] = temp[1];
-            } else {
-                nums[i] = temp[2];
-                nums[i+1] = temp[0];
-                nums[i+2] = temp[1];
-            }
-            lessOrder = !lessOrder;
         }
     }
 }
