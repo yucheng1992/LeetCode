414e940faa5679106f48ce43703657b242c506a9
yl2612 <yl2695@nyu.edu>
2015-10-22 16:06:51 -0400

    add a simple method for missing number

--- LeetCode/MissingNumber.java
+++ LeetCode/MissingNumber.java
@@ -15,4 +15,13 @@ public class MissingNumber {
         }
         return nums.length;
     }
+
+    //simple sum 
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return (nums.length + 1) * nums.length / 2 - sum;
+    }
 }
